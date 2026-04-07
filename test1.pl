0c5f478adcd32f5fe968b38b18adbb7a5d91760c
AlanMV <mv.alan4806@gmail.com>
2026-04-06 21:47:31 -0600

    Add comments and database

--- test1.pl
+++ test1.pl
@@ -1,1 +1,14 @@
+% data base:  Declararemos los productos con sus atributos.
+% vamos a seguir la siguiente estructura: producto(Tipo, Marca, Producto, Presentacion, Precio, Comentario)
+
+% Despensa y Alimentos
+% Aceites
+producto('Aceite', 'Nutrioli', 'Canola', '1L', 40, 'N/A').
+producto('Aceite', 'Crystal', 'Vegetal', '1L', 35, 'N/A').
+producto('Aceite', 'Capullo', 'Soya', '840ml', 50, 'N/A').
+
+
+
+
+
 writeHelloWorld :- write('Hello Wordl').
