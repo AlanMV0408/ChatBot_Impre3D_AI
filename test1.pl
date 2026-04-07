da8c506f2fa8ea9317df0364e598859358fd581d
AlanMV <mv.alan4806@gmail.com>
2026-04-06 22:23:58 -0600

    Add more products of Despensa y Alimentos

--- test1.pl
+++ test1.pl
@@ -2,10 +2,27 @@
 % vamos a seguir la siguiente estructura: producto(Tipo, Marca, Producto, Presentacion, Precio, Comentario)
 
 % Despensa y Alimentos
+
 % Aceites
 producto('Aceite', 'Nutrioli', 'Canola', '1L', 40, 'N/A').
 producto('Aceite', 'Crystal', 'Vegetal', '1L', 35, 'N/A').
 producto('Aceite', 'Capullo', 'Soya', '840ml', 50, 'N/A').
+
+% Pastas 
+producto('Pastas', 'La Moderna', 'Estrellita', '250gr', 10.00, 'N/A').
+producto('Pastas', 'Barilla', 'Fusilli', '450gr', 23.50, 'N/A').
+producto('Pastas', 'Great Value', 'Codo', '500gr', 16.00, 'N/A').
+
+% Arroz
+
+producto('Arroz', 'Verde Valle', 'Grano grueso', '750gr', 35.00, 'N/A').
+producto('Arroz', 'Progreso', 'Super extra', '1kg', 38.50, 'N/A').
+producto('Arroz', 'Bueno', 'Super extra', '907gr', 27.00, 'N/A').
+
+
+
+
+
 
 
 
