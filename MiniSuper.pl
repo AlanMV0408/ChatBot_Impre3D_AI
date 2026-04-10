b513fc83d2089ee8afd38ec5c2f39ce677731d2f
AlanMV <mv.alan4806@gmail.com>
2026-04-09 23:05:55 -0600

    Add new category Cremeria y Carnes

--- MiniSuper.pl
+++ MiniSuper.pl
@@ -1,7 +1,7 @@
 % data base:  Declararemos los productos con sus atributos.
 % vamos a seguir la siguiente estructura: producto(Tipo, Marca, Producto, Presentacion, Precio, Comentario)
 
-% Despensa y Alimentos
+% ---------- Despensa y Alimentos ----------
 
 % Aceites
 producto('Aceite', 'Nutrioli', 'Canola', '1L', 40, 'N/A').
@@ -14,10 +14,30 @@ producto('Pastas', 'Barilla', 'Fusilli', '450gr', 23.50, 'N/A').
 producto('Pastas', 'Great Value', 'Codo', '500gr', 16.00, 'N/A').
 
 % Arroz
-
 producto('Arroz', 'Verde Valle', 'Grano grueso', '750gr', 35.00, 'N/A').
 producto('Arroz', 'Progreso', 'Super extra', '1kg', 38.50, 'N/A').
 producto('Arroz', 'Bueno', 'Super extra', '907gr', 27.00, 'N/A').
+
+% ---------- Cremeria y Carnes ----------
+
+% Crema
+producto('Crema', 'Sello Rojo', 'Acidificada Pasteurizada', '900gr', 80.00, 'N/A').
+producto('Crema', 'LALA', 'Entera', '900gr', 99.00, 'N/A').
+producto('Crema', 'Alpura', 'Acidificada Pasteurizada', '900gr', 75.00, 'N/A').
+
+% Jamon
+producto('Jamon', 'ROS', 'Pavo', '500gr', 55.00, 'N/A').
+producto('Jamon', 'FUD', 'Cerdo', '430gr', 76.00, 'N/A').
+producto('Jamon', 'San Rafael', 'Pierna', '300gr', 88.00, 'N/A').
+
+% Salchicha
+producto('Salchicha', 'ROS', 'Pavo', '500gr', 50.00, 'N/A').
+producto('Salchicha', 'FUD', 'Pavo', '500gr', 50.00, 'N/A').
+producto('Salchicha', 'LALA', 'Pavo', '500gr', 70.00, 'N/A').
+
+
+
+
 
 
 
