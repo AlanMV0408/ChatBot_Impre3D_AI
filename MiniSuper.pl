780dafcf9d8e72d75fe2f7e2ce390faeb374013b
AlanMV <mv.alan4806@gmail.com>
2026-04-12 21:50:32 -0600

    Add all categories with products (data base complete)

--- MiniSuper.pl
+++ MiniSuper.pl
@@ -1,63 +1,208 @@
 % data base:  Declararemos los productos con sus atributos.
 % vamos a seguir la siguiente estructura: producto(Tipo, Marca, Producto, Presentacion, Precio, Comentario)
 
-% ---------- Despensa y Alimentos ----------
+
+% ---------- Snacks --------- 
+
+% Cacahuates
+producto('Cacahuates', 'Mafer', 'Japones con Limon', '180gr', 45.00, 'N/A').
+producto('Cacahuates', 'Sabritas', 'Enchilados', '150gr', 22.00, 'N/A').
+producto('Cacahuates', 'Kacang', 'Cubiertos de Chile', '185gr', 38.00, 'N/A').
+
+% Papas
+producto('Papas', 'Sabritas', 'Original Sal', '160gr', 58.00, 'N/A').
+producto('Papas', 'Barcel', 'Takis Fuego', '190gr', 52.00, 'N/A').
+producto('Papas', 'Pringles', 'Original', '124gr', 55.00, 'N/A').
+
+% Chicles
+producto('Chicles', 'Trident', 'Menta Val-U-Pack', '18s', 25.00, 'N/A').
+producto('Chicles', 'Clorets', 'Plus', '12s', 18.00, 'N/A').
+producto('Chicles', 'Orbit', 'Spearmint', '14s', 22.00, 'N/A').
+
+% Galletas
+producto('Galletas', 'Gamesa', 'Marias', '510gr', 48.00, 'N/A').
+producto('Galletas', 'Marinela', 'Principe', '252gr', 35.00, 'N/A').
+producto('Galletas', 'Oreo', 'Original', '273gr', 42.00, 'N/A').
+
+% Mentas
+producto('Mentas', 'Tic Tac', 'Menta', '16gr', 15.00, 'N/A').
+producto('Mentas', 'Halls', 'Extra Fuerte', '9 piezas', 12.00, 'N/A').
+producto('Mentas', 'Mentos', 'Pure Fresh', '28gr', 28.00, 'N/A').
+
+% Chocolate
+producto('Chocolate', 'Hersheys', 'Leche', '40gr', 22.00, 'N/A').
+producto('Chocolate', 'Ferrero Rocher', '3 Pack', '37.5gr', 35.00, 'N/A').
+producto('Chocolate', 'Nestle', 'Carlos V', '18gr', 10.00, 'N/A').
+
+
+% ---------- Despensa y Alimentos ---------- 
 
 % Aceites
 producto('Aceite', 'Nutrioli', 'Canola', '1L', 40, 'N/A').
 producto('Aceite', 'Crystal', 'Vegetal', '1L', 35, 'N/A').
 producto('Aceite', 'Capullo', 'Soya', '840ml', 50, 'N/A').
 
-% Pastas 
-producto('Pastas', 'La Moderna', 'Estrellita', '250gr', 10.00, 'N/A').
-producto('Pastas', 'Barilla', 'Fusilli', '450gr', 23.50, 'N/A').
-producto('Pastas', 'Great Value', 'Codo', '500gr', 16.00, 'N/A').
-
 % Arroz
 producto('Arroz', 'Verde Valle', 'Grano grueso', '750gr', 35.00, 'N/A').
 producto('Arroz', 'Progreso', 'Super extra', '1kg', 38.50, 'N/A').
 producto('Arroz', 'Bueno', 'Super extra', '907gr', 27.00, 'N/A').
 
-% ---------- Cremeria y Carnes ----------
+% Frijol
+producto('Frijol', 'Isadora', 'Bayos Refritos', '430gr', 22.50, 'N/A').
+producto('Frijol', 'Verde Valle', 'Negro Querétaro', '1kg', 54.00, 'N/A').
+producto('Frijol', 'La Sierra', 'Bayos', '560gr', 20.00, 'N/A').
+
+% Pastas 
+producto('Pastas', 'La Moderna', 'Estrellita', '250gr', 10.00, 'N/A').
+producto('Pastas', 'Barilla', 'Fusilli', '450gr', 23.50, 'N/A').
+producto('Pastas', 'Great Value', 'Codo', '500gr', 16.00, 'N/A').
+
+% Cafe
+producto('Cafe', 'Nescafe', 'Clasico', '120gr', 95.00, 'N/A').
+producto('Cafe', 'Andatti', 'Americano Molido', '454gr', 110.00, 'N/A').
+producto('Cafe', 'Legal', 'Soluble con Azucar', '180gr', 55.00, 'N/A').
+
+% Azucar
+producto('Azucar', 'Zulka', 'Estandar', '1kg', 42.00, 'N/A').
+producto('Azucar', 'Great Value', 'Refinada', '1kg', 48.00, 'N/A').
+
+% Pan
+producto('Pan', 'Bimbo', 'Blanco Grande', '680gr', 55.00, 'Pan de caja').
+producto('Pan', 'Wonder', 'Integral', '567gr', 52.00, 'Pan de caja').
+producto('Pan', 'Bimbo', 'Donas Espolvoreadas', '105gr', 25.00, 'Pan dulce').
+
+% Salsas / Aderezos
+producto('Salsas', 'La Costeña', 'Salsa Casera', '450gr', 24.00, 'Preparada').
+producto('Salsas', 'Herdez', 'Salsa Verde', '445gr', 26.00, 'Preparada').
+producto('Aderezos', 'McCormick', 'Mayonesa con Limon', '390gr', 58.00, 'N/A').
+
+
+% ---------- Hogar y Limpieza ---------- 
+
+% Detergente en Polvo 
+producto('Detergente Polvo', 'Roma', 'Biodegradable', '1kg', 35.00, 'N/A').
+producto('Detergente Polvo', 'Foca', 'Biodegradable', '1kg', 40.00, 'N/A').
+producto('Detergente Polvo', 'Salvo', 'Biodegradable Multiusos', '1kg', 36.00, 'N/A').
+
+% Blanqueadores
+producto('Blanqueador', 'Cloralex', 'El Rendidor', '950ml', 18.50, 'N/A').
+producto('Blanqueador', 'Clorox', 'Anti-esplash', '930ml', 22.00, 'N/A').
+
+% Papel Higienico
+producto('Detergente Polvo', 'Petalo', 'Ultraresistente', '4pz', 33.00, 'N/A').
+producto('Detergente Polvo', 'Vogue', 'Rendidor', '4pz', 27.00, 'N/A').
+producto('Detergente Polvo', 'Premier', 'MAX', '4pz', 35.00, 'N/A').
+
+% Platos
+producto('Platos', 'Reyma', 'Pastelero No. 6', '20 piezas', 25.00, 'Desechable').
+producto('Platos', 'Great Value', 'Extendidos', '20 piezas', 38.00, 'Desechable').
+producto('Platos', 'Dart', 'Termico No. 9', '20 piezas', 42.00, 'Unicel').
+
+% Vasos
+producto('Vasos', 'Reyma', 'Plastico No. 12', '25 piezas', 32.00, 'Desechable').
+producto('Vasos', 'Dart', 'Termico No. 8', '25 piezas', 30.00, 'Unicel').
+
+
+% ---------- Cuidado personal y Farmacia ---------- 
+
+% Maquillaje
+producto('Maquillaje', 'Maybelline', 'Mascara Great Lash', '12.7gr', 145.00, 'N/A').
+producto('Maquillaje', 'Bissú', 'Polvo Compacto', '8gr', 85.00, 'N/A').
+producto('Maquillaje', 'Vogue', 'Base Resist', '30ml', 160.00, 'N/A').
+
+% Cremas
+producto('Cremas', 'Nivea', 'Milk Nutritiva', '400ml', 85.00, 'Humectante').
+producto('Cremas', 'Lubriderm', 'Piel Normal', '400ml', 98.00, 'Humectante').
+producto('Cremas', 'Dove', 'Nutricion Esencial', '400ml', 92.00, 'Humectante').
+
+% Shampoo
+producto('Shampoo', 'Pantene', 'Control Caida', '700ml', 110.00, 'N/A').
+producto('Shampoo', 'Head & Shoulders', 'Limpieza Renovadora', '700ml', 115.00, 'N/A').
+producto('Shampoo', 'Sedal', 'Restauracion Instantanea', '620ml', 75.00, 'N/A').
+
+% Pañales
+producto('Pañales', 'Huggies', 'Ultra Confort Etapa 4', '40 piezas', 280.00, 'N/A').
+producto('Pañales', 'Pampers', 'Cruisers Etapa 5', '30 piezas', 310.00, 'N/A').
+producto('Pañales', 'KleenBebé', 'Suavelastic Etapa 3', '38 piezas', 195.00, 'N/A').
+
+% Paracetamol
+producto('Paracetamol', 'Genomma Lab', 'Tabletas 500mg', '10 piezas', 25.00, 'N/A').
+producto('Paracetamol', 'Tempra', 'Tabletas 500mg', '20 piezas', 65.00, 'N/A').
+producto('Paracetamol', 'Great Value', 'Tabletas 500mg', '20 piezas', 20.00, 'N/A').
+
+% Desodorante
+producto('Desodorante', 'Rexona', 'Men Invisible', '150ml', 65.00, 'N/A').
+producto('Desodorante', 'Axe', 'Black', '150ml', 68.00, 'N/A').
+producto('Desodorante', 'Dove', 'Original', '150ml', 72.00, 'N/A').
+
+% Antiacido
+producto('Antiacido', 'Tums', 'Surtido de Frutas', '12 tabletas', 45.00, 'N/A').
+producto('Antiacido', 'Pepto-Bismol', 'Suspension', '236ml', 115.00, 'N/A').
+
+
+% ---------- Cremeria y Carnes ---------- 
 
 % Crema
 producto('Crema', 'Sello Rojo', 'Acidificada Pasteurizada', '900gr', 80.00, 'N/A').
 producto('Crema', 'LALA', 'Entera', '900gr', 99.00, 'N/A').
 producto('Crema', 'Alpura', 'Acidificada Pasteurizada', '900gr', 75.00, 'N/A').
 
-% Jamon
-producto('Jamon', 'ROS', 'Pavo', '500gr', 55.00, 'N/A').
-producto('Jamon', 'FUD', 'Cerdo', '430gr', 76.00, 'N/A').
-producto('Jamon', 'San Rafael', 'Pierna', '300gr', 88.00, 'N/A').
-
 % Salchicha
 producto('Salchicha', 'ROS', 'Pavo', '500gr', 50.00, 'N/A').
 producto('Salchicha', 'FUD', 'Pavo', '500gr', 50.00, 'N/A').
 producto('Salchicha', 'LALA', 'Pavo', '500gr', 70.00, 'N/A').
 
+% Jamon
+producto('Jamon', 'ROS', 'Pavo', '500gr', 55.00, 'N/A').
+producto('Jamon', 'FUD', 'Cerdo', '430gr', 76.00, 'N/A').
+producto('Jamon', 'San Rafael', 'Pierna', '300gr', 88.00, 'N/A').
 
-% ---------- Hogar y Limpieza ----------
+% Chorizo
+producto('Chorizo', 'FUD', 'Chorizo de Pavo', '200gr', 32.00, 'N/A').
+producto('Chorizo', 'Bafar', 'Chorizo de Cerdo', '200gr', 28.00, 'N/A').
+producto('Chorizo', 'LALA', 'Chorizo de Cerdo', '200gr', 35.00, 'N/A').
 
-% Detergente en Polvo 
-% producto('Detergente Polvo', 'Roma', 'Biodegradable', '1kg', 35.00, 'N/A').
-% producto('Detergente Polvo', 'Foca', 'Biodegradable', '1kg', 40.00, 'N/A').
-% producto('Detergente Polvo', 'Salvo', 'Biodegradable Multiusos', '1kg', 36.00, 'N/A').
+% Queso
+producto('Queso', 'LALA', 'Panela Fresco', '400gr', 78.00, 'N/A').
+producto('Queso', 'Nochebuena', 'Manchego Rebanado', '400gr', 115.00, 'N/A').
+producto('Queso', 'FUD', 'Oaxaca', '400gr', 92.00, 'N/A').
 
-% Papel Higienico
-% producto('Detergente Polvo', 'Petalo', 'Ultraresistente', '4pz', 33.00, 'N/A').
-% producto('Detergente Polvo', 'Vogue', 'Rendidor', '4pz', 27.00, 'N/A').
-% producto('Detergente Polvo', 'Premier', 'MAX', '4pz', 35.00, 'N/A').
+% Leche
+producto('Leche', 'LALA', 'Entera', '1L', 28.00, 'N/A').
+producto('Leche', 'Alpura', 'Clasica', '1L', 27.50, 'N/A').
+producto('Leche', 'Santa Clara', 'Entera', '1L', 32.00, 'N/A').
 
-
-
-
-
-
-
+% Mantequilla
+producto('Mantequilla', 'Lala', 'Con Sal', '90gr', 22.00, 'N/A').
+producto('Mantequilla', 'Primavera', 'Margarina con Sal', '190gr', 38.00, 'N/A').
+producto('Mantequilla', 'Gloria', 'Sin Sal', '90gr', 25.00, 'N/A').
 
 
+% ---------- Bebidas ---------- 
 
+% Refresco
+producto('Refresco', 'Coca Cola', 'Original', '600ml', 19.00, 'N/A').
+producto('Refresco', 'Pepsi', 'Original', '600ml', 17.00, 'N/A').
+producto('Refresco', 'Jarritos', 'Tutti Frutti', '600ml', 15.00, 'N/A').
 
+% Agua
+producto('Agua', 'Bonafont', 'Natural', '1L', 16.00, 'N/A').
+producto('Agua', 'Ciel', 'Natural', '1L', 15.00, 'N/A').
+producto('Agua', 'E-pura', 'Natural', '1.5L', 18.00, 'N/A').
+
+% Suero
+producto('Suero', 'Electrolit', 'Coco', '625ml', 32.00, 'N/A').
+producto('Suero', 'Suerox', 'Mora Azul', '630ml', 28.00, 'N/A').
+
+% Cerveza
+producto('Cerveza', 'Corona', 'Extra', '355ml', 22.00, 'N/A').
+producto('Cerveza', 'Victoria', 'Vichy', '473ml', 25.00, 'Lata').
+producto('Cerveza', 'Heineken', 'Original', '355ml', 28.00, 'N/A').
+
+% Vinos
+producto('Vinos', 'Concha y Toro', 'Casillero del Diablo', '750ml', 240.00, 'Tinto').
+producto('Vinos', 'L.A. Cetto', 'Cabernet Sauvignon', '750ml', 190.00, 'Tinto').
+producto('Vinos', 'Riunite', 'Lambrusco', '750ml', 165.00, 'Rosado').
 
 
 
